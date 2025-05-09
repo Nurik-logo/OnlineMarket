735d191082be50b4e16c6ac8d68c46d32e2bb54a
Nurik-logo <adaesadaesa@gmail.com>
2025-05-10 00:08:37 +0500

    fix: use node instead of nodemon for Render

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const storeCategoriesRoutes = require('./routes/storeCategories');
 const transactionsRoutes = require('./routes/transactions');
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: "db.env" });
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
